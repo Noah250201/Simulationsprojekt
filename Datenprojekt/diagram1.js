92ee83cc81e80e9a62f1738836a733641581807e
MaxBühler <buehler.max@icloud.com>
2025-02-26 19:02:07 +0100

    diagram1 just needs proper data now, maybe title for the axis? diagram2 and 3 dummies for now

--- Datenprojekt/diagram1.js
+++ Datenprojekt/diagram1.js
@@ -2,10 +2,12 @@
 var jsonfile = {
 "jsonarray": [{
     "stockprice":"112",
-    "date":"16.1.23"},
+    "date":"16.1.23",
+    "googleViewCount":"1000"},
 {
     "stockprice":"122",
-    "date":"15.2.23",},
+    "date":"15.2.23",
+    "googleViewCount":"2000"},
 {
     "stockprice":"124",
     "date":"14.3.23"
@@ -13,8 +15,11 @@ var jsonfile = {
 ]
 }; //die Variable wird später durch die Response ersetzt
 
-var data = jsonfile.jsonarray.map(function(e){
+var data1 = jsonfile.jsonarray.map(function(e){
     return e.stockprice;
+});
+var data2 = jsonfile.jsonarray.map(function(e){
+    return e.googleViewCount;
 });
 var labels = jsonfile.jsonarray.map(function(e){
     return e.date;
@@ -26,10 +31,45 @@ var config = {
     data: {
         labels: labels,
         datasets: [{
-            label: 'birthdates',
-            data: data
+            label: 'Stock Prices',
+            data: data1,
+            yAxisID: 'y',
+            borderColor: 'blue',
+            fill: false
+        }, {
+            label: 'Google View Count',
+            data: data2,
+            yAxisID: 'y1',
+            borderColor: 'red',
+            fill: false
         }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+          mode: 'index',
+          intersect: false,
+        },
+        stacked: false,
+        scales: {
+          y: {
+            type: 'linear',
+            display: true,
+            position: 'left',
+          },
+          y1: {
+            type: 'linear',
+            display: true,
+            position: 'right',
+    
+            // grid line settings
+            grid: {
+              drawOnChartArea: false, // only want the grid lines for one axis to show up
+            },
+        }
     }
-}
+  }
+};
+
 
 var chart = new Chart(ctx, config);
