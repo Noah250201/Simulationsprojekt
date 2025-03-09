22ae186b3aca976b3ac49f17fad00588b0d6535c
OneRelax <relax1511.builder@gmail.com>
2025-03-09 20:41:52 +0100

    cleaned up Main; created HTMLServerController.java created RESTServerController.java

--- ProjectBackEnd/src/main/java/org/dataprojectNHMT/Main.java
+++ ProjectBackEnd/src/main/java/org/dataprojectNHMT/Main.java
@@ -1,60 +1,17 @@
 package org.dataprojectNHMT;
 
-import io.javalin.Javalin;
-import org.dataprojectNHMT.controller.GoogleTrendController;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.dataprojectNHMT.controller.HTMLServerController;
+import org.dataprojectNHMT.controller.RESTServerController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().get("/", ctx -> ctx.result("No Whitelable Error, because Hello World")).start(8080);
-        log.info("Javalin has been initialized.");
-        setup(app);
-        startingJetty();
-    }
-
-    private static void setup(Javalin app){
-        GoogleTrendController googleTrendController = new GoogleTrendController();
-        app.get("/trendData", ctx -> ctx.result(googleTrendController.getTrendsData()));
-
-        log.info("javalin REST-Endpoints have been setup.");
-    }
-
-    private static void startingJetty() {
-        int port = 4200;
-
-        log.debug("Creating a Jetty server on port {}", port);
-        Server server = new Server(port);
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-
-        String resourceBase = "../Datenprojekt";
-        context.setResourceBase(resourceBase);
-        context.addServlet(DefaultServlet.class, "/");
-
-        try {
-            server.start();
-            log.info("Server started at http://localhost:{}", port);
-
-            Desktop desktop = Desktop.getDesktop();
-            desktop.browse(URI.create("http://localhost:" + port));
-            log.info("Browser opened at address http://localhost:{}",port);
-
-        } catch (IOException e) {
-            log.error("Failed to open browser at http://localhost:{}", port, e);
-        } catch (Exception e) {
-            log.error("Front-End Jetty Server failed to start",e);
-        }
+        RESTServerController rest = new RESTServerController(8080);
+        log.debug("Finished creating RESTServerController.");
+        HTMLServerController html = new HTMLServerController(4200);
+        log.debug("Finished creating HTMLServerController.");
     }
 }
---
+++ ProjectBackEnd/src/main/java/org/dataprojectNHMT/controller/RESTServerController.java
@@ -0,0 +1,28 @@
+package org.dataprojectNHMT.controller;
+
+import io.javalin.Javalin;
+import org.dataprojectNHMT.services.GoogleTrendService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RESTServerController {
+
+    private static final Logger log = LoggerFactory.getLogger(RESTServerController.class);
+    
+    private final Javalin server;
+
+    public RESTServerController(int port) {
+        server = Javalin.create().start(port);
+        log.info("Started Back-End Server on port {}", port);
+
+        setupRestEndpoints();
+        log.info("Front-End Server REST-Endpoints have been setup.");
+    }
+    
+    private void setupRestEndpoints() {
+        server.get("/", endpoint -> endpoint.result("No Whitelable Error, because Hello World"));
+        
+        GoogleTrendService googleTrendService = new GoogleTrendService();
+        server.get("/trendData", endpoint -> endpoint.result(googleTrendService.getTrendsData()));
+    }
+}
