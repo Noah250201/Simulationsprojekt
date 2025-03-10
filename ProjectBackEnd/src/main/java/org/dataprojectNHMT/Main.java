b6c293d41c9e65d41af0a4a818ae6feaa91efa9b
OneRelax <relax1511.builder@gmail.com>
2025-03-10 21:09:39 +0100

    added first DatabaseConnection

--- ProjectBackEnd/src/main/java/org/dataprojectNHMT/Main.java
+++ ProjectBackEnd/src/main/java/org/dataprojectNHMT/Main.java
@@ -2,6 +2,7 @@ package org.dataprojectNHMT;
 
 import org.dataprojectNHMT.controller.HTMLServerController;
 import org.dataprojectNHMT.controller.RESTServerController;
+import org.dataprojectNHMT.database.DatabaseConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,7 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        DatabaseConnection db = new DatabaseConnection();
         RESTServerController rest = new RESTServerController(8080);
         log.debug("Finished creating RESTServerController.");
         HTMLServerController html = new HTMLServerController(4200);
---
+++ ProjectBackEnd/src/main/java/org/dataprojectNHMT/database/DatabaseConnection.java
@@ -0,0 +1,59 @@
+package org.dataprojectNHMT.database;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConnection.class);
+
+    private Connection connection;
+
+    public DatabaseConnection() {
+        String databaseUrl = "jdbc:h2:mem:datenprojekt";
+        try {
+            connection = DriverManager.getConnection(databaseUrl);
+            setup();
+        } catch (SQLException e) {
+            log.error("Failed to connect to Database with Connection-String {}", databaseUrl, e);
+        }
+    }
+
+    private void setup() throws SQLException {
+        Statement statement = connection.createStatement();
+
+        statement.execute("Create Table stock(" +
+                "stockID integer primary key," +
+                "symbol varchar(20) NOT NULL," +
+                "date DATE NOT NULL," +
+                "price double);");
+        statement.executeQuery("Create Table publisher( " +
+                "publisherID integer primary key," +
+                "publisherName String Not Null," +
+                "courseID fk" +
+                "analytics fk" +
+                "publisher_games fk);");
+        statement.executeQuery("Create Table publisher_games(" +
+                "publisher_gamesID integer pk," +
+                "publisherID fk" +
+                "gamesID fk);");
+        statement.executeQuery("Create Table analytics(" +
+                "analyticsID integer primary key," +
+                "month Date NOT NULL," +
+                "searches integer," +
+                "gameID" +
+                "publisherID);");
+        statement.executeQuery("Create Table games(" +
+                "gameID integer primary key," +
+                "gameName String," +
+                "owners String," +
+                "initialPrice double," +
+                "currentPrice double," +
+                "avgPlayerForever integer," +
+                "avgPlayerLast2Weeks integer ," +
+                "scoreRank integer," +
+                "publisher_games ID" +
+                "analytics ID);");
+    }
+}
