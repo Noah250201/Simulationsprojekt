47437be0b48be5c92723cd14eed8e29b796dfc0a
OneRelax <relax1511.builder@gmail.com>
2025-03-15 12:09:54 +0100

    added static JsonMapper; added static lastDateOnRecord to Main;

--- ProjectBackEnd/src/main/java/org/dataprojectNHMT/Main.java
+++ ProjectBackEnd/src/main/java/org/dataprojectNHMT/Main.java
@@ -6,8 +6,12 @@ import org.dataprojectNHMT.controller.DatabaseController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDate;
+
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static final LocalDate lastDateOnRecord = LocalDate.now();
 
     public static void main(String[] args) {
         DatabaseController db = new DatabaseController();
