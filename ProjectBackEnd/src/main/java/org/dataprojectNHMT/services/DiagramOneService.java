0040aa19e50b27b21315ee0e25d33890f484524c
OneRelax <relax1511.builder@gmail.com>
2025-03-16 15:43:42 +0100

    implemented DiagramOneService method

--- ProjectBackEnd/src/main/java/org/dataprojectNHMT/services/DiagramOneService.java
+++ ProjectBackEnd/src/main/java/org/dataprojectNHMT/services/DiagramOneService.java
@@ -1,7 +1,18 @@
 package org.dataprojectNHMT.services;
 
+import org.dataprojectNHMT.Main;
 import org.dataprojectNHMT.controller.DatabaseController;
 import org.dataprojectNHMT.dtos.in.InputOneDTO;
+import org.dataprojectNHMT.dtos.out.DiagramDTO;
+import org.dataprojectNHMT.dtos.out.DiagramOneDTO;
+import org.dataprojectNHMT.entitys.GameEntity;
+import org.dataprojectNHMT.entitys.PublisherEntity;
+import org.dataprojectNHMT.entitys.StockEntity;
+import org.dataprojectNHMT.util.JsonMapper;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiagramOneService {
 
@@ -12,6 +23,27 @@ public class DiagramOneService {
     }
 
     public String getDiagram(InputOneDTO input) {
-        return "";
+        List<DiagramDTO> dtoList = new ArrayList<>();
+        List<GameEntity> gameList = input.getGames().stream().map(db::getGameByName).toList();
+        PublisherEntity publisher = db.getPublisherByName(input.getPublisher());
+        LocalDate date = Main.lastDateOnRecord;
+
+        for (GameEntity game : gameList) {
+            DiagramOneDTO dto = new DiagramOneDTO();
+            StockEntity stock = db.getStockByDateAndPublisher(date,publisher);
+            dto.setGame(game.getGameName());
+            dto.setAveragePlayers(game.getAveragedPlayersLastTwoWeeks());
+            dto.setDate(date);
+            dto.setStockprice(stock.getPrice());
+            dtoList.add(dto);
+
+            date = switch (input.getInterval()) {
+                case "wöchentlich" -> date.minusWeeks(1);
+                case "monatlich" -> date.minusMonths(1);
+                default -> date.minusDays(1);
+            };
+        }
+
+        return JsonMapper.mapDTOsToArray(dtoList);
     }
 }
